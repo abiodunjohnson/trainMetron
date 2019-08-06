6835f10e4a38396ec1ce444ba9a7ac2bd527e4f3
Donifez <donifez224@gmail.com>
2019-08-06 09:22:56 -0700

    auth

--- auth.js
+++ auth.js
@@ -1,3 +1,4 @@
+window.onload=function (){
 const signupForm=document.querySelector("#signupform");
 signupForm.addEventListener('submit',(e)=>{
     e.preventDefault();
@@ -8,25 +9,60 @@ const fullname= signupForm['signupfullname'].value;
  
 //sign up the user
 auth.createUserWithEmailAndPassword(email, password).then( cred=> {
-  
-    signupForm.reset();
+ 
+ window.location='index.html';
 })
 
 
 })
+}
+
 
 
 //login
+window.onload=function (){
+    
 const signinForm= document.querySelector("#signinform");
 
 signinForm.addEventListener('submit', (e)=>{
     e.preventDefault();
 
     //get user info
+   
     const email= signinForm['login-email'].value;
     const password=signinForm['login-password'].value;
+
+
     auth.signInWithEmailAndPassword(email,password).then(cred=> {
         console.log(cred.user);
-        signinForm.reset();
+       
+        // signinForm.reset();
+        if (cred.user==='auth/wrong-password') {
+          alert("error");
+            
+        } else {
+            window.location='index.html';
+        }
+     
     })
+
+//     firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+//         // Handle Errors here.
+//         var errorCode = error.code;
+      
+//         // [START_EXCLUDE]
+//         if (errorCode === 'auth/wrong-password') {
+//           alert('Wrong password.');
+//         } else {
+//             window.location='index.html';
+//         }
+        
+    
+//         // [END_EXCLUDE]
+//       });
+   
+
+
+  
 })
+}
